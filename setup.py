08f776f98e565099d641dcb1bde7c6804e2ea896
reddy-j-harshith <f20220233@hyderabad.bits-pilani.ac.in>
2024-06-13 17:19:09 +0530

    furthur setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from gettext import install
+from setuptools import setup, find_packages
+
+setup(
+    name = 'Munnabh-AI',
+    version = '0.0.0',
+    author = ['Harshith Reddy',
+               'Jaitra Saketh'],
+    packages = find_packages(),
+    install_requires = []
+
+)
